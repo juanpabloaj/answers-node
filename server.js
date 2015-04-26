ae26a321b1d3d9d22d4d16615ece5365d104d42f
JuanPablo <jpabloaj@gmail.com>
2015-04-26 12:10:45 -0400

    code is verified as child process

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var Firebase = require('firebase');
+var exec = require('child_process').exec;
+var fs = require('fs');
 
 var firebaseUrl = process.env.FIREBASE_URL;
 
@@ -8,23 +10,38 @@ var questionsRef= new Firebase(firebaseUrl + 'questions');
 answersRef.limitToLast(1).on('value', function(snap){
   snap.forEach(function(ans){
     if ( ans.val().state === 'waiting' ) {
+      console.log('Processing answers ' + ans.key());
       var question = ans.val().question;
 
       questionsRef.child(question).on('value', function(ques){
         var expected = ques.val().expected;
-        var evaluated = String(eval(ans.val().code));
+        var question_input = ques.val().input;
+        var code = ans.val().code;
 
-        var state = 'failed';
-        if ( expected === evaluated ) {
-          state = 'passed';
-        }
+        var script_content = question_input + ';\n' + code;
+        fs.writeFileSync('script.js', script_content);
 
-        answersRef.child(ans.key()).update({
-          'state':state
-        }, function(err){
-          if (err) console.log(err);
-          process.exit(0);
+        exec('node ./script.js', function(error, stdout, stderr){
+
+          if ( error !== null ) {
+            console.log('exec error: ', error);
+          }
+
+          var state = 'failed';
+          if ( expected === stdout ) {
+            state = 'passed';
+          }
+
+          answersRef.child(ans.key()).update({
+            'state':state,
+            'output': stdout,
+            'error': stderr
+          }, function(err){
+            if (err) console.log(err);
+            process.exit(0);
+          });
         });
+
       });
 
     }
@@ -34,4 +51,4 @@ answersRef.limitToLast(1).on('value', function(snap){
 setTimeout(function(){
   console.log("It's time to sleep!");
   process.exit(0);
-}, 2000);
+}, 4000);
