a1b5d511279bc000369de35ab7d6083e04181b40
JuanPablo <jpabloaj@gmail.com>
2015-10-21 20:42:15 -0300

    python3.4 support

--- test/utils.js
+++ test/utils.js
@@ -3,8 +3,12 @@ var utils = require('../lib/utils');
 
 describe('utils', function(){
   describe('gen_script_name', function(){
-    it('python2.7 return python2.7', function(){
+    it('python2.7 return script.py', function(){
       assert.equal('script.py', utils.gen_script_name('python2.7'));
+    });
+
+    it('python3.4 return script.py', function(){
+      assert.equal('script.py', utils.gen_script_name('python3.4'));
     });
 
     it('javascript return node', function(){
@@ -16,9 +20,14 @@ describe('utils', function(){
     });
   });
   describe('gen_exec_command', function(){
-    it('python2.7 return python script.py', function(){
+    it('python2.7 return python2.7 script.py', function(){
       assert.equal('python2.7 script.py', utils.gen_exec_command('python2.7'));
     });
+
+    it('python3.4 return python3.4 script.py', function(){
+      assert.equal('python3.4 script.py', utils.gen_exec_command('python3.4'));
+    });
+
     it('javascript return node script.js', function(){
       assert.equal('node script.js', utils.gen_exec_command('javascript'));
     });
